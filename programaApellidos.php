a5fb7a1677779ebca0256426af5aeb96cd65c2b3
tomisanchezz <tomassanchez2018@gmail.com>
2023-11-08 22:07:56 -0300

    funcion 2 TOMAS

--- programaApellidos.php
+++ programaApellidos.php
@@ -7,6 +7,7 @@ include_once("wordix.php");
 /***** DATOS DE LOS INTEGRANTES *******/
 /**************************************/
 // Sanchez, TOMAS. FAI-4494. TUDW. tomas.sanchez2004@est.fi.uncoma.edu.ar. tomisanchezz.
+
 /* Apellido, Nombre. Legajo. Carrera. mail. Usuario Github */
 /* ****COMPLETAR***** */
 
@@ -15,6 +16,7 @@ include_once("wordix.php");
 /***** DEFINICION DE FUNCIONES ********/
 /**************************************/
 
+/**1)Funcion cargarColeccionPalabras */
 /**
  * Obtiene una colección de palabras
  * @return array
@@ -24,16 +26,32 @@ function cargarColeccionPalabras()
     $coleccionPalabras = [
         "MUJER", "QUESO", "FUEGO", "CASAS", "RASGO",
         "GATOS", "GOTAS", "HUEVO", "TINTO", "NAVES",
-        "VERDE", "MELON", "YUYOS", "PIANO", "PISOS"
-        /* Agregar 5 palabras más */
+        "VERDE", "MELON", "YUYOS", "PIANO", "PISOS", 
+        "ARBOL", "MUNDO", "PERRO", "VACAS", "MOUSE"
+        
     ];
 
     return ($coleccionPalabras);
 }
 
-/* ****COMPLETAR***** */
+/* ****COMPLETAR CON TODAS LAS FUNCIONES***** */
 
+function cargarPartidas(){
+$partida1=["palabraWordix "=> "TINTO" , "jugador" => "Tomas", "intentos"=> 1, "puntaje" => 14];
+$partida2=["palabraWordix "=> "MOUSE" , "jugador" => "Tomas", "intentos"=> 2, "puntaje" => 13];
+$partida3=["palabraWordix "=> "PERRO" , "jugador" => "Tomas", "intentos"=> 4, "puntaje" => 13];
+$partida4=["palabraWordix "=> "ARBOL" , "jugador" => "Tomas", "intentos"=> 3, "puntaje" => 13];
+$partida5=["palabraWordix "=> "GATOS" , "jugador" => "Tomas", "intentos"=> 6, "puntaje" => 0];
+$partida6=["palabraWordix "=> "VERDE" , "jugador" => "Tomas", "intentos"=> 3, "puntaje" => 14];
+$partida7=["palabraWordix "=> "MELON" , "jugador" => "Tomas", "intentos"=> 2, "puntaje" => 15];
+$partida8=["palabraWordix "=> "HUEVO" , "jugador" => "Tomas", "intentos"=> 5, "puntaje" => 9];
+$partida9=["palabraWordix "=> "GOTAS" , "jugador" => "Tomas", "intentos"=> 2, "puntaje" => 15];
+$partida10=["palabraWordix "=> "MUJER" , "jugador" => "Tomas", "intentos"=> 0, "puntaje" => 0];
 
+$coleccionPartidas= [$partida1,$partida2,$partida3,$partida4,$partida5,$partida6,$partida7,$partida8,$partida9,$partida10];
+
+return $coleccionPartidas;
+} 
 
 /**************************************/
 /*********** PROGRAMA PRINCIPAL *******/
